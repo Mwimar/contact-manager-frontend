20ffbfc04689c3c77a1bc026160689db04f8ff3c
Mark Mwima <markmwimar@gmail.com>
2024-05-02 12:24:38 +0300

    signup function connected to backend

--- scripts/users.js
+++ scripts/users.js
@@ -1,4 +1,13 @@
-async function signUp() {
+const signupFormElement = document.querySelector("#login form");
+
+async function signUp(event) {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  const userData = {};
+  formData.forEach((value, key) => {
+    userData[key] = value;
+  });
+
   let response;
   try {
     response = await fetch("http://localhost:5001/api/users/register", {
@@ -6,9 +15,20 @@ async function signUp() {
       headers: {
         "Content-type": "application/json",
       },
+      body: JSON.stringify(userData),
     });
   } catch (error) {
     alert("Something went wrong");
     return;
   }
+
+  if (!response.ok) {
+    alert("Something went wrong");
+    return;
+  }
+
+  const responseData = await response.json();
+  console.log(responseData);
 }
+
+signupFormElement.addEventListener("submit", signUp);
