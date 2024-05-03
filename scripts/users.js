38bd9976a5a51f4fe50dd3d1f813b20299245538
Mark Mwima <markmwimar@gmail.com>
2024-05-03 13:33:10 +0000

    login function

--- scripts/users.js
+++ scripts/users.js
@@ -32,4 +32,32 @@ async function signUp(event) {
   window.location.href = "http://localhost:5500/index.html";
 }
 
+async function login(event) {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  const userData = {};
+  formData.forEach((value, key) => {
+    userData[key] = value;
+  });
+
+  let response;
+  try {
+    response = await fetch("http://localhost:5001/api/users/login", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(userData),
+    });
+  } catch (error) {
+    alert("something went wrong");
+    return;
+  }
+
+  if (!response.ok) {
+    alert("Something Went Wrong");
+    return;
+  }
+}
+
 signupFormElement.addEventListener("submit", signUp);
