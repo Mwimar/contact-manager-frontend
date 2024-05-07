ed67aae86712fbbd69e1f7a348776cb3606883b8
Mark Mwima <markmwimar@gmail.com>
2024-05-07 14:15:34 +0300

    loading user specific data

--- scripts/contacts/contacts.js
+++ scripts/contacts/contacts.js
@@ -1,12 +1,14 @@
 async function loadContacts() {
-  // if (event && event.preventDefault) {
-  //   event.preventDefault(); // Prevent default action (e.g., form submission)
-  // }
+  const token = localStorage.getItem("token");
 
   let response;
 
   try {
-    response = await fetch("http://localhost:5001/api/contacts/");
+    response = await fetch("http://localhost:5001/api/contacts/", {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
   } catch (error) {
     alert("Something went wrong");
     console.log("FAILED TO FETCH DATA");
@@ -28,26 +30,3 @@ async function loadContacts() {
 }
 
 loadContacts();
-
-// async function loadContacts() {
-//   try {
-//     const response = await fetch("https://localhost:5001/api/contacts", {
-//       method: "GET",
-//       headers: {
-//         "Content-Type": "application/json", // corrected header name
-//       },
-//     });
-
-//     if (!response.ok) {
-//       throw new Error("Failed to fetch contacts");
-//     }
-
-//     const responseData = await response.json(); // Parse response as JSON
-//     console.log(responseData); // Output the parsed data
-//   } catch (error) {
-//     console.error("Something went wrong:", error.message);
-//     // Handle the error appropriately (e.g., show error message to the user)
-//   }
-// }
-
-// loadContacts();
